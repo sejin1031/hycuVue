2922882df8ab38ef0bb2ef655108b8b6d5e08533
sejin.eom <sejin1031@utplus.co.kr>
2020-08-29 18:01:32 +0900

    TodoList 예제

---
+++ src/components/Todo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="todo-container">
+    <div class="title">제목: {{ this.todo.title }}</div>
+    <div class="todo-content">내용: {{ this.todo.content }}</div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoComp',
+  props: [ 'todo' ]
+}
+</script>
+
+<style>
+.todo-container{
+  margin-bottom: 30px;
+  border: 3px black solid;
+}
+.title { font-size: 30pt; color: blue; }
+.todo-content { font-size: 45pt; color: green; }
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="todo-list">
+    <div class="new-content">
+      제목 : <input v-model="titleText"> <br/>
+      내용 : <input v-model="contentText" @keypress.enter="addList"> <br/>
+      <button @click="addList">
+        추가</button>
+    </div>
+    <div class="todo-wrap">
+      <todo :key="index" v-for="(data,index) in listData" :todo="data"><button @click="removeList(index)">삭제</button></todo>
+    </div>
+  </div>
+</template>
+
+<script>
+import Todo from '@/components/Todo'
+export default {
+  name: 'TodoList',
+  components: {
+    todo: Todo
+  },
+  data () {
+    return {
+      titleText: '',
+      contentText: '',
+      listData: [],
+    }
+  },
+  methods: {
+    addList : function () {
+      var todo = {
+        title: this.titleText,
+        content: this.contentText
+      };
+      this.listData.push(todo);
+      this.titleText = '';
+      this.contentText = '';
+    },
+    removeList : function (index) {
+      this.listData.splice(index,1)
+    }
+  }
+}
+</script>
+
+<style scoped>
+#todolist {
+  font-weight: bold;
+}
+.todo-wrap {
+  border: black 3px solid; width: 30vw; left: auto; right: auto;
+}
+</style>
