577897ec7da18d3efc1c77a7ec7d19546d5558ec
sejin.eom <sejin1031@utplus.co.kr>
2020-09-06 12:51:57 +0900

    실습용 브랜치

---
+++ src/components/TodoList.vue
@@ -0,0 +1,30 @@
+<template>
+  
+</template>
+
+<script>
+import '@/components/Todo.vue' // @는 src/와 동일한 의미
+export default {
+  name: 'TodoList',
+  data () { // data 속성은 반드시 함수형 (return이 되도록)
+    return {
+      todolist: [],
+      titleText: '',
+      contentText: ''
+    }
+  },
+  methods: {
+    addTodo: function () {
+      
+    },
+    removeTodo: function () {
+      
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
