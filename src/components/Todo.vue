6957c6d5a743ac547234e99614b815ec73c26adf
sejin.eom <sejin1031@utplus.co.kr>
2020-09-06 12:53:29 +0900

    Todo 스타일 추가

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -10,5 +10,10 @@ export default {
 </script>
 
 <style>
-
+.todo-container{
+  margin-bottom: 30px;
+  border: 3px black solid;
+}
+.title { font-size: 30pt; color: blue; }
+.todo-content { font-size: 45pt; color: green; }
 </style>
